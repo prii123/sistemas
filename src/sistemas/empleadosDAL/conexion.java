3e32f679326426894bfdbd6b3f2d97c6928af904
prii123 <83259348+prii123@users.noreply.github.com>
2022-06-03 17:30:06 -0500

    java conexion a sqlite

---
+++ src/sistemas/empleadosDAL/conexion.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemas.empleadosDAL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class conexion {
+    
+    String strConexionDB = "jdbc:sqlite:D:/Programacion/Programing/JAVA/sistemas/SISTEMA.db";
+    Connection conn = null;
+    
+    public conexion(){
+        
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection(strConexionDB);
+            
+            System.out.println("Conexion establecida");
+        } catch (Exception e) {
+            
+            System.out.println("Error de conexion"+e);
+        }
+    
+    }
+    
+    
+    public int ejecutarSentenciaSQL(String strSentenciaSQL){
+        try {
+            
+            PreparedStatement pstm = conn.prepareStatement(strSentenciaSQL);
+            pstm.execute();
+            
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return 0;
+        }
+    }
+    
+    
+    public ResultSet consultarRegistros(String strSentenciaSQL){
+        try {
+            PreparedStatement pstm = conn.prepareStatement(strSentenciaSQL);
+            ResultSet respuesta = pstm.executeQuery();
+            return respuesta;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    
+    }
+    
+}
