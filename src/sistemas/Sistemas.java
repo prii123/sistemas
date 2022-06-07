8c96bebf03de7694a9a4559ba1b9a1736f82c31e
prii123 <83259348+prii123@users.noreply.github.com>
2022-06-06 19:02:47 -0500

    interfaz empresas

--- src/sistemas/Sistemas.java
+++ src/sistemas/Sistemas.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package sistemas;
+import javax.swing.JFrame;
+import sistemas.GUI.frmInicio;
 
 /**
  *
@@ -15,6 +17,9 @@ public class Sistemas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        JFrame f = new frmInicio();
+        f.setVisible (true);
     }
     
 }
